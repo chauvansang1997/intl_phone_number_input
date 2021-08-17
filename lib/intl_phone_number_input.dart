c831ce5c6a1d9304435a12975a892a95e9cb81fc
Chau Van Sang <chauvansang97@gmail.com>
2021-08-17 20:16:01 +0700

    update export for external get country

--- lib/intl_phone_number_input.dart
+++ lib/intl_phone_number_input.dart
@@ -1,5 +1,11 @@
 library intl_phone_number_input;
 
+export 'src/models/country_list.dart';
+export 'src/models/country_model.dart';
+export 'src/providers/country_provider.dart';
+export 'src/utils/formatter/as_you_type_formatter.dart';
 export 'src/utils/phone_number.dart';
-export 'src/widgets/input_widget.dart';
 export 'src/utils/selector_config.dart';
+export 'src/widgets/countries_search_list_widget.dart';
+export 'src/widgets/input_widget.dart';
+export 'src/utils/phone_number/phone_number_util.dart';
